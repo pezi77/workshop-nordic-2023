de7624b303f00b941ee3b463e15b614d5beb2778
pezi77 <49612300+pezi77@users.noreply.github.com>
2023-08-24 16:07:45 +0200

    Update conf.py with personal info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Nordic TechKomm'
+copyright = '2023, Petra'
+author = 'Petra'
 
 release = '0.1'
 version = '0.1.0'
